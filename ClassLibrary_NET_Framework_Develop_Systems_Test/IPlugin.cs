0d3bd090efbff63be408040e761e16a3df3b28cb
Бевз Роман Юрьевич <bevz.riu@students.dvfu.ru>
2020-10-06 19:23:27 +0300

    Попытки терпят крах

---
+++ ClassLibrary_NET_Framework_Develop_Systems_Test/IPlugin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds.test.impl
+{
+	public interface IPlugin
+	{
+		string PluginName { get; }
+		string Version { get; }
+		Image Image { get; }
+		string Description { get; }
+		int Run(int input1, int input2);
+	}
+
+	abstract class Property : IPlugin
+	{
+		private string _pluginName;
+		public string PluginName { get => _pluginName; }
+		private string _version;
+		public string Version { get => _version; }
+
+		public Image Image { get => Image; }
+		private string _description;
+		public string Description { get => _description; }
+
+		public int Run(int input1, int input2)
+		{
+			return 0;
+		}
+	}
+	//class Add
+	//{
+	//	public static Add operator +(Add input1, Add input2)
+	//	{
+	//		return new Add { Property.Value = input1.Value + input2.Value };
+	//	}
+	//}
+}
---
+++ ClassLibrary_NET_Framework_Develop_Systems_Test/Plugins.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds.test.impl
+{
+	public static class Plugins
+	{
+		public interface IPluginFactory
+		{
+			int PluginsCount { get; }
+			string[] GetPluginNames { get; }
+			IPlugin GetPlugin(string pluginName);
+		}
+
+		public static double Add(Plugins.IPluginFactory int input1, Plugins.IPluginFactory int input2)
+		{
+			return input1 + input2;
+		}
+	}
+
+
+	//	Console.WriteLine("Какой-то текст");
+	//	Console.ReadKey();
+	//}
+}
